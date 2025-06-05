7e8a9de4c83188265b192bc2e2611b039bc92c8f
suresh <126891547+Nandhagopal2003@users.noreply.github.com>
2025-06-05 18:29:19 +0530

    Initial commit

---
+++ chatapp/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:chatapp/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Talky App',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          elevation: 1,
+          iconTheme: IconThemeData(color: const Color.fromARGB(255, 0, 0, 0)),
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 19,
+          ),
+          backgroundColor: const Color.fromARGB(255, 193, 193, 224),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
